6b6bdf33bbbd0749b625a66d89885fce689a3b31
Adityaksinghh <152866092+Adityaksinghh@users.noreply.github.com>
2023-12-10 14:13:57 +0530

    Add files via upload

---
+++ ques2.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    x = 5, y = 5;
+    int sum = x + y;
+    cout<<sum;
+    }
---
+++ ques3.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"size of int: "<<sizeof(int)<<" bytes"<<endl;
+    cout<<"size of bool: "<<sizeof(bool)<<" bytes"<<endl;
+    cout<<"size of float: "<<sizeof(float)<<" bytes"<<endl;
+}
